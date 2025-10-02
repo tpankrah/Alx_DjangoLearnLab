092c9dccdddbbb90d9aacd1bd9486dfc756b4edd
Theophilus Ankrah <tpankkrah@live.com>
2025-10-02 03:34:54 +0000

    updated class view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
-    path('books/',views.books, name='books')
+    path('books/',views.books, name='books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,9 +10,11 @@ def books(request):
     return render(request,'relationship_app/list_books.html',{"books":books})
 
 
-class BookDetailView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    #context_object_name = "library"
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
