342c1c6584ee2c6707337b3bcf5ab559ce595b6d
Theophilus Ankrah <tpankkrah@live.com>
2025-10-02 03:41:25 +0000

    updated class view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Book
 from .models import Library
-from django.views.generic import DetailViews
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def books(request):
