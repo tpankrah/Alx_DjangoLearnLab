9efe98cffe3cdf2e39865d5be9349acd226fa12c
Theophilus Ankrah <tpankkrah@live.com>
2025-10-02 02:46:59 +0000

    django function veiws

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Book
 
 # Create your views here.
+def books(request):
+    books = Book.objects.all()
+
+    output = []
+    for book in books:
+        output.append(f"{book.title} by {book.author}")
+    
+    return HttpResponse(output)
+ 
