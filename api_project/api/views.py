c15e5b58bfd5df6b8631dd56653027ec4fb8d786
Theophilus Ankrah <tpankkrah@live.com>
2025-10-06 06:37:07 +0000

    implementing viewsets and routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 #from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics , viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,3 +8,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
